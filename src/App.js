2ea356021d0655d8088bfd9a81f4c70ff6c952d3
katereclark <cla16022@byui.edu>
2023-11-14 12:19:51 -0700

    Fixed routing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import List from "./pages/List";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/food-index">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/list" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
